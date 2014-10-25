e4aa66cf4f5e9cbdf63b883192f80c2a18f9f8c1
kemingchen <believe75467@gmail.com>
2014-10-26 01:55:04 +0800

    add TestMindMap Solution

---
+++ TestMindMap/TestMindMap.cpp
@@ -0,0 +1,10 @@
+// TestMindMap.cpp : 定義主控台應用程式的進入點。
+//
+#include "stdafx.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
+    system("pause");
+}
