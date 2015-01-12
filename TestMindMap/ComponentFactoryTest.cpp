493e4de38a469d032a6703ff7b57b3113c33de6f
kemingchen <believe75467@gmail.com>
2015-01-12 23:53:09 +0800

    add tests for createDecorate

--- TestMindMap/ComponentFactoryTest.cpp
+++ TestMindMap/ComponentFactoryTest.cpp
@@ -21,9 +21,9 @@ TEST_F(ComponentFactoryTest, createComponent)
 {
     for (int i = 0; i < 2; i++)
     {
-        Component* root = _factory->createComponent(ROOT, "Computer");
-        Component* nodeOS = _factory->createComponent(NODE, "OS");
-        Component* nodeWindows = _factory->createComponent(NODE, "Windows");
+        Component* root = _factory->createComponent(ComponentType::ROOT, "Computer");
+        Component* nodeOS = _factory->createComponent(ComponentType::NODE, "OS");
+        Component* nodeWindows = _factory->createComponent(ComponentType::NODE, "Windows");
         ASSERT_EQ("Root", root->getTypeName());
         ASSERT_EQ(0, root->getId());
         ASSERT_EQ("Computer", root->getDescription());
@@ -33,5 +33,25 @@ TEST_F(ComponentFactoryTest, createComponent)
         ASSERT_EQ("Node", nodeWindows->getTypeName());
         ASSERT_EQ(2, nodeWindows->getId());
         ASSERT_EQ("Windows", nodeWindows->getDescription());
+    }
+}
+
+TEST_F(ComponentFactoryTest, createDecorate)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        Component* root = _factory->createComponent(ComponentType::ROOT, "Computer");
+        Component* node = _factory->createComponent(ComponentType::NODE, "Node");
+        Component* ellipse = _factory->createDecorate("Ellipse", root);
+        Component* triangle = _factory->createDecorate("Triangle", ellipse);
+        Component* rectangle = _factory->createDecorate("Rectangle", triangle);
+        ellipse->addChild(node);
+        rectangle->removeChild(node);
+        triangle->addChild(node);
+        rectangle->removeAllChild();
+        ASSERT_EQ("Ellipse", ellipse->getTypeName());
+        ASSERT_EQ("Triangle", triangle->getTypeName());
+        ASSERT_EQ("Rectangle", rectangle->getTypeName());
+        ASSERT_EQ("Computer", rectangle->getRealComponent()->getDescription());
     }
 }
